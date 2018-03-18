f0d184639cea0830538c2f8b6e01a22fac4a7776
jens <jens@garage.dk>
2018-03-18 17:33:34 +0100

    Made data file element, possibly need to remove sol file

---
+++ Data.py
@@ -0,0 +1,63 @@
+class Data:
+    Courses_max = None
+    rooms_max = None
+    days_max = None
+    Periods_per_day = None
+    Curricula_max = None
+    constraints_max = None
+    lecturers_max = None
+    C_q = None
+    T_d = None
+    L_c = None
+    S_c = None
+    M_c = None
+    C_r = None
+    F_ct= None
+
+
+
+
+    def __init__(self, data):
+        self.basic = data['basic']
+        self.courses = data['courses']
+        self.rooms =data['rooms']
+        self.relation = data['relation']
+        self.unavailability = data['unavailability']
+
+        # Basic data
+        self.Courses_max = int(self.basic[1][0])
+        self.rooms_max = int(self.basic[1][1])
+        self.days_max = int(self.basic[1][2])
+        self.Periods_per_day = int(self.basic[1][3])
+        self.Curricula_max = int(self.basic[1][4])
+        self.constraints_max = int(self.basic[1][5])
+        self.lecturers_max = int(self.basic[1][6])
+        self.total_timeslots = self.days_max * self.Periods_per_day
+
+    def set_C_q(self,C_q):
+        assert isinstance(C_q, list)
+        self.C_q = C_q
+
+    def set_T_d(self,T_d):
+        assert isinstance(T_d, list)
+        self.T_d = T_d
+
+    def set_S_c(self,S_c):
+        assert isinstance(S_c, list)
+        self.S_c = S_c
+
+    def set_M_c(self,M_c):
+        assert isinstance(M_c, list)
+        self.M_c = M_c
+
+    def set_L_c(self,L_c):
+        assert isinstance(L_c, list)
+        self.L_c = L_c
+
+    def set_C_r(self,C_r):
+        assert isinstance(C_r, list)
+        self.C_r = C_r
+
+    def set_F_ct(self,F_ct):
+        assert isinstance(F_ct, list)
+        self.F_ct = F_ct
--- TimeTableImport.py
+++ TimeTableImport.py
@@ -1,7 +1,7 @@
 import os
 from __global__ import *
 from Solution import *
-
+from Data import *
 
 
 
@@ -9,56 +9,69 @@ from Solution import *
 def file_names(DIR):
     files = [name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]
     return  sorted(files, key=str.lower)
+
 DIR = "data/Test01/"
 files = file_names(DIR)
-
 #print(files)
 def read_file(files):
-    data = []
+    data = {}
     for i in files:
         print(i)
         with open("data/Test01/"+i) as infile:
             RAW = infile.readlines()
-        vars()[i[:-4]] = [i.rsplit() for i in RAW]
-        data.append(vars()[i[:-4]])
-    #print(data)
+        temp = [j.rsplit() for j in RAW]
+        data[i[:-4]]= temp
+    print(data)
     return data
 
-basic,courses,curricula,lecturers,relation,rooms,unavailabiliy= read_file(files)
-print(basic)
-print(courses)
-print(curricula)
-print(lecturers)
-print(relation)
-print(rooms)
-print(unavailabiliy)
+data= read_file(files)
+print(data.keys())
 
 
-Sol = Solution(basic)
+def Set_params(data):
 
-print(Sol.Curricula)
+    C_q = [[] for q in range(data.Curricula_max)]
+    for i, j in data.relation[1:]:
+        C_q[int(i[1:])].append(int(j[1:]))
 
-C_q = [[] for q in range(Sol.Curricula)]
-for i,j in relation[1:]:
-    C_q[int(i[1:])].append(int(j[1:]))
+    C_q = [[] for q in range(data.Curricula_max)]
+    for i, j in data.relation[1:]:
+        C_q[int(i[1:])].append(int(j[1:]))
 
-T_d = [Sol.Periods_per_day * i for i in range(Sol.days)]
+    T_d = [data.Periods_per_day * i for i in range(data.days_max)]
 
-L_c = [None for i in range (Sol.Courses)]
-S_c = [0 for i in range(Sol.Courses)]
-M_c = [0 for i in range(Sol.Courses)]
-for i,j,k,l,m in courses[1:]:
-    L_c[int(i[1:])]=int(k)
-    S_c[int(i[1:])]=int(m)
-    M_c[int(i[1:])] = int(l)
+    L_c = [None for i in range(data.Courses_max)]
+    S_c = [0 for i in range(data.Courses_max)]
+    M_c = [0 for i in range(data.Courses_max)]
+    for i, j, k, l, m in data.courses[1:]:
+        L_c[int(i[1:])] = int(k)
+        S_c[int(i[1:])] = int(m)
+        M_c[int(i[1:])] = int(l)
 
-C_r = [0 for i in range(Sol.rooms)]
-for i,j in rooms[1:]:
-    C_r[int(i[1:])]=int(j)
+    C_r = [0 for i in range(data.rooms_max)]
+    for i, j in data.rooms[1:]:
+        C_r[int(i[1:])] = int(j)
 
-F_ct = [[True for i in range(Sol.total_timeslots) ] for j in range(Sol.Courses)]
-for i,j,k in unavailabiliy[1:]:
-    F_ct[int(i[1:])][int(j)*Sol.Periods_per_day+int(k)-1] = False
+    F_ct = [[True for i in range(data.total_timeslots)] for j in range(data.Courses_max)]
+    for i, j, k in data.unavailability[1:]:
+        F_ct[int(i[1:])][int(j) * data.Periods_per_day + int(k) - 1] = False
+    print(type(C_q))
+    data.set_C_q(C_q)
+    data.set_C_r(C_r)
+    data.set_L_c(L_c)
+    data.set_S_c(S_c)
+    data.set_M_c(M_c)
+    data.set_F_ct(F_ct)
 
 
-print(F_ct[0][23])
+
+
+#Sol = Solution(basic)
+
+data = Data(read_file(files))
+
+print(data)
+Set_params(data)
+
+
+print(data.C_q)
